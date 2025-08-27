900d31831d21d2c151d19247e4b86399bf69fc31
vourse <aditrap388@gmail.com>
2025-08-27 14:16:32 +0700

    initial

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public $table = 'products';
+
+    public $fillable = [
+        
+    ];
+
+    protected $casts = [
+        
+    ];
+
+    public static array $rules = [
+        
+    ];
+
+    
+}
